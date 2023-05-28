915b5dcac9b78eb4666ccd49cb24cb7e47a3a89f
KIRSTEN-23 <231296@virtualwindow.co.za>
2023-05-28 20:47:47 +0200

    Exercise 3

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let numOne = 30
+let numTwo = 15
+let numThree = 40
+let numFour = 0
+
+let total = numOne + numTwo / numThree * numFour
+
+let answer = "The answer is " + total
+
+console.log(answer)
